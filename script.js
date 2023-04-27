5483d625f5a281e427367942bdb31da68ad9d788
Deepak Sharma <75806982+dipu-developer@users.noreply.github.com>
2023-04-27 10:40:56 +0530

    upload project

---
+++ script.js
@@ -0,0 +1,50 @@
+// var peragraph =2
+
+
+    const getPera = (pessage) => {
+        const url =
+        "https://lorem-ipsum-by-api-ninjas.p.rapidapi.com/v1/loremipsum?paragraphs=" +
+        pessage;
+        const options = {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/octet-stream',
+                'X-RapidAPI-Key': '71d0ff669dmshaa3f8057890b028p15ce70jsn633f919746bb',
+                'X-RapidAPI-Host': 'lorem-ipsum-by-api-ninjas.p.rapidapi.com'
+            }
+        };
+    fetch(url, options)
+      .then((resp) => resp.json())
+      .then((res) => {
+        document.getElementById("text").value = res.text;
+      }).catch ((error) =>console.error(error))
+}
+
+const but = document.getElementById("submit");
+const copy =document.getElementById("copy");
+const a = document.getElementById("number");
+but.addEventListener("click", function () {
+    var b = a.value;
+    if (b>0){
+        getPera(b);
+        document.getElementById("warning").style.display="none";
+    }else{
+        console.log("invalid input")
+        document.getElementById("warning").style.display="block";
+        
+    }
+});
+copy.addEventListener("click", function () {
+  var txt = document.getElementById("text")
+  txt.select();
+  navigator.clipboard.writeText(txt.value);
+  alert("Peragraph is copy")
+  
+//   getPera(a);
+});
+a.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("submit").click();
+    }
+});
